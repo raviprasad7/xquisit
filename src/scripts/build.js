f671cc53f1cab379c5bdf8f310b15258ad7a4f7d
Ravi Prasad <ravi070994@gmail.com>
2023-04-28 00:16:11 +0530

    Built transpiling component's scss into css

--- src/scripts/build.js
+++ src/scripts/build.js
@@ -2,11 +2,34 @@ const Fs = require('fs');
 const Path = require('path');
 const Sass = require('sass');
 
-const result = Sass.renderSync({
-  data: Fs.readFileSync(Path.resolve('src/global.scss')).toString(),
-  outputStyle: 'expanded',
-  outFile: 'global.css',
-  includePaths: [Path.resolve('src')],
-});
+const getComponents = () => {
+  let allComponents = [];
 
-Fs.writeFileSync(Path.resolve('src/lib/global.css'), result.css.toString());
+  const types = ['atoms', 'molecules', 'organisms'];
+
+  types.forEach((type) => {
+    const allFiles = Fs.readdirSync(`src/${type}`).map((file) => ({
+      input: Path.resolve('src', type, file),
+      output: `src/lib/${file.slice(0, -4)}` + 'css',
+    }));
+
+    allComponents = [...allComponents, ...allFiles];
+  });
+
+  return allComponents;
+};
+
+const compile = (path, fileName) => {
+  const result = Sass.renderSync({
+    data: Fs.readFileSync(Path.resolve(path)).toString(),
+    outputStyle: 'expanded',
+    outFile: 'global.css',
+    includePaths: [Path.resolve('src')],
+  }).css.toString();
+
+  Fs.writeFileSync(Path.resolve(fileName), result);
+};
+
+compile('src/global.scss', 'src/lib/global.css');
+
+getComponents().forEach(({ input, output }) => compile(input, output));
